38bb2cc73ec0af225c7af1e9ee3058a22c6432a0
Shanice Dunk <shanice.dunk@icloud.com>
2024-08-15 21:02:52 +0100

    Added comments to Drinks file

--- src/Drinks.java
+++ src/Drinks.java
@@ -1,19 +1,21 @@
 public class Drinks {
     public static int linearSearch(String[] arr, String target) {
-        // loop through list to find element
+        // loop through list to find the drink in the array
         for (int i = 0; i < arr.length; i ++) {
             if (arr[i].equals(target)) {
                 System.out.println("This drink is available.");
                 return i;
             }
         }
-        // Print statement if element is not found
+        // Print statement if drink is not available
             System.out.println("This drink is not available.");
             return -1;
     }
 
     public static void main(String[] args) {
+        // Array list of drinks 
         String arr[] = {"Prosecco", "White Wine", "Red Wine", "Rosé", "Beer", "Cider", "Spirits", "Non-alcoholic"};
+        // Search for drink
         String target = "White Wine";
         linearSearch(arr, target);
     }
